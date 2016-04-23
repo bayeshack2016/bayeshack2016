fd66d3aa4996bbfb98720a7fab5923a2a90c3a3f
Victoria T. Gu <victoriatingtinggu@gmail.com>
2016-04-23 16:56:26 -0700

    send text back

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import request
 import json
+import requests
 import sys
 import traceback
 app = Flask(__name__)
@@ -11,7 +12,6 @@ def main():
 
 @app.route('/webhook', methods=['GET', 'POST'])
 def validate():
-    token = "CAAYM4P71Md8BAL1BzNrSPZCHnbZAVjvmfJwI4KSdFDRXxREVLUfacPKGP05GA82LGYkZCMDYXPb09k4yV04Rv8b0xtOXtj3aqFZA8ApW8D4v8zvuIbNHsJ4ydXsLq8XxKDTt0xgVnvrQTe44ArduAZARQ2YwAYEZAJhzs29oQjtExRvRup3ZACyhZCSj4EoZBKi4ZD"
     try:
         if request.method == 'GET':
             if request.args.get('hub.verify_token') == 'verify_me':
@@ -29,14 +29,25 @@ def validate():
             for event in messages:
                 # sender = event.sender.id
                 if (event.get('message') and event['message']['text']):
+                    sender_id = event['sender']['id']
                     text = event['message']['text']
                     # Handle a text message from this sender
                     sys.stderr.write(text + '\n')
+                    sys.stderr.write(str(sender_id) + '\n')
+                    sendTextMessage(sender_id, text)
                     return text
     except:
         exc_type, exc_value, exc_traceback = sys.exc_info()
         app.logger.error(traceback.print_exception(exc_type, exc_value, exc_traceback, limit=2))
         return 'Error, wrong validation token'
+
+def sendTextMessage(sender, text):
+    token = "CAAYM4P71Md8BAMRPqQyRa7pHVvQyXydWi2LRzZBVoZBPr8qxhLi9ZC1kD26PgFx3IB1E17tTjECLFGn4uerwISpVzuLMmqIaGSgvyMNgF0ix1RZA4x1VGPiKMXCRzJ1B7xH2D2G0fuHGeAroC4gVX8qqxz2aaTbusYz5P82arcy4oV6ZAScwKcZBWfcMauYpUZD"
+    url = "https://graph.facebook.com/v2.6/me/messages"
+    payload = dict(recipient=dict(id=sender), message=text, qs=dict(access_token=token))
+    r = requests.post(url, data=payload)
+    sys.stderr.write(str(r.status_code) + '\n')
+
 
 # def sendTextMessage(sender, text):
 #     messageData = {text=text}
