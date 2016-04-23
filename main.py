817640f1969d822fea24eaea6f7e25a7d5dcbded
Victoria T. Gu <victoriatingtinggu@gmail.com>
2016-04-23 14:32:35 -0700

    Add webhook

--- main.py
+++ main.py
@@ -5,5 +5,11 @@ app = Flask(__name__)
 def main():
     return "Hello World!"
 
+@app.route('/webhook', methods=['GET'])
+def validate(req, res):
+    if req.query['hub.verify_token'] == 'verify_me':
+        res.send(req.query['hub.challenge']);
+    res.send('Error, wrong validation token');
+
 if __name__ == "__main__":
     app.run()
