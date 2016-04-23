eaea95ffe0d0239e49c7ead844043a653261ce53
Victoria T. Gu <victoriatingtinggu@gmail.com>
2016-04-23 16:18:19 -0700

    add stderr logging

--- main.py
+++ main.py
@@ -11,27 +11,49 @@ def main():
 
 @app.route('/webhook', methods=['GET', 'POST'])
 def validate():
+    token = "CAAYM4P71Md8BAL1BzNrSPZCHnbZAVjvmfJwI4KSdFDRXxREVLUfacPKGP05GA82LGYkZCMDYXPb09k4yV04Rv8b0xtOXtj3aqFZA8ApW8D4v8zvuIbNHsJ4ydXsLq8XxKDTt0xgVnvrQTe44ArduAZARQ2YwAYEZAJhzs29oQjtExRvRup3ZACyhZCSj4EoZBKi4ZD"
     try:
         if request.method == 'GET':
-            return request.args.get('hub.challenge')
-        else:
             if request.args.get('hub.verify_token') == 'verify_me':
-                app.logger.error(request.args)
-                app.logger.error(request.data)
-                app.logger.error(request.form)
-                app.logger.error(request.values)
-                messaging_events = json.loads(request.data.decode("utf-8"))
-                for i in xrange(len(messaging_events)):
-                    event = messaging_events[i]
-                    # sender = event.sender.id
-                    if (event['message'] and event['message']['text']):
-                        text = event['message']['text']
-                        # Handle a text message from this sender
-                        return text
+                return request.args.get('hub.challenge')
+        else:
+            sys.stderr.write('stderr\n')
+            sys.stdout.write('stdout\n')
+            app.logger.error(request.args)
+            app.logger.error(request.data)
+            app.logger.error(request.form)
+            app.logger.error(request.values)
+            messaging_events = json.loads(request.data.decode("utf-8"))
+            for i in xrange(len(messaging_events)):
+                event = messaging_events[i]
+                # sender = event.sender.id
+                if (event['message'] and event['message']['text']):
+                    text = event['message']['text']
+                    # Handle a text message from this sender
+                    return text
     except:
         exc_type, exc_value, exc_traceback = sys.exc_info()
         app.logger.error(traceback.print_exception(exc_type, exc_value, exc_traceback, limit=2))
         return 'Error, wrong validation token'
+
+# def sendTextMessage(sender, text):
+#     messageData = {text=text}
+#     request({
+#         url: 'https://graph.facebook.com/v2.6/me/messages',
+#         qs: {access_token:token},
+#         method: 'POST',
+#         json: {
+#         recipient: {id:sender},
+#         message: messageData,
+#         }
+#     }, function(error, response, body) {
+#         if (error) {
+#         console.log('Error sending message: ', error);
+#         } else if (response.body.error) {
+#         console.log('Error: ', response.body.error);
+#         }
+#     });
+#     }
 
 if __name__ == "__main__":
     app.run(debug=True)
