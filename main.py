caa4a009f572dbf7eb3a30bcef6e2223c491330a
Victoria T. Gu <victoriatingtinggu@gmail.com>
2016-04-23 15:41:28 -0700

    Add request json decoding

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import request
+import json
 import sys
 import traceback
 app = Flask(__name__)
@@ -8,15 +9,29 @@ app = Flask(__name__)
 def main():
     return "Hello World!"
 
-@app.route('/webhook', methods=['GET'])
+@app.route('/webhook', methods=['GET', 'POST'])
 def validate():
     try:
-        if request.args.get('hub.verify_token') == 'verify_me':
+        if request.method == 'GET':
             return request.args.get('hub.challenge')
+        else:
+            if request.args.get('hub.verify_token') == 'verify_me':
+                app.logger.error(request.args)
+                app.logger.error(request.data)
+                app.logger.error(request.form)
+                app.logger.error(request.values)
+                messaging_events = json.loads(request.data.decode("utf-8"))
+                for i in xrange(len(messaging_events)):
+                    event = messaging_events[i]
+                    # sender = event.sender.id
+                    if (event['message'] and event['message']['text']):
+                        text = event['message']['text']
+                        # Handle a text message from this sender
+                        return text
     except:
         exc_type, exc_value, exc_traceback = sys.exc_info()
         app.logger.error(traceback.print_exception(exc_type, exc_value, exc_traceback, limit=2))
-    return 'Error, wrong validation token'
+        return 'Error, wrong validation token'
 
 if __name__ == "__main__":
     app.run(debug=True)
