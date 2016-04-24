ff8ab6db3856826d2c88bab6a4f3f8032f655061
Alex Mansourati <alex@format.com>
2016-04-23 20:17:08 -0700

    had dinner without commit. dont remember what i did. too lazy for diff

--- model/data_clean.py
+++ model/data_clean.py
@@ -1,13 +1,23 @@
 import numpy as np
+import pandas as pd
+from helpers import indicate_outlier_values, get_income
+
+df = pd.read_csv('temp_df.csv')
+
+cols_to_use = ['educ', 'age', 'female', 'occ12', 'smsastat14', 'wage1', 'wage2', 'wage3','wage4', 'weekpay', 'pehrusl1']
+
+#Pre-processing
+df = df[cols_to_use]
+df['outlier'] = df.apply(indicate_outlier_values, axis=1)
+df = df[df.outlier == 0]
+df['income'] = df.apply(get_income, axis=1)
+df.dropna(subset=['income'], inplace=True)
 
 
-df = pd.io.stata.read_stata('cepr_org_2015.dta', convert_categoricals=False, convert_missing=True)
+df.drop(['wage1', 'wage2', 'wage3', 'wage4', 'weekpay', 'pehrusl1'], axis=1, inplace=True)
 
+df.to_csv('final_df.csv')
 
-cols_to_use = ['educ']
+#df.educ.map(educ_map, inplace=True)
+#educ_dummies = pd.get_dummies(df.educ)
 
-
-df.educ.map(educ_map)
-
-
-pd.to_dummies()
--- model/maps.py
+++ model/maps.py
@@ -1,6 +1,6 @@
-educ_map = {1: "LTHS"
-			2: "HS"
-			3: "Some college"
-			4: "College"
+educ_map = {1: "LTHS",
+			2: "HS",
+			3: "Some college",
+			4: "College",
 			5: "Advanced"}
 
