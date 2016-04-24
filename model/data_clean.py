52bacae46673c827d276ebcd1117ec67e011af04
Alex Mansourati <alex@format.com>
2016-04-24 01:41:37 -0700

    working model

--- model/data_clean.py
+++ model/data_clean.py
@@ -1,23 +1,37 @@
 import numpy as np
 import pandas as pd
-from helpers import indicate_outlier_values, get_income
+from fuzzywuzzy import process
+from maps import job_title_map, city_name_map, gender_map
+from job_ids import ordered_job_ids
+from city_ids import ordered_city_ids
+from model_parameters import coefficients, fields
 
-df = pd.read_csv('temp_df.csv')
+def predict_income(city, job, age, education, gender):
 
-cols_to_use = ['educ', 'age', 'female', 'occ12', 'smsastat14', 'wage1', 'wage2', 'wage3','wage4', 'weekpay', 'pehrusl1']
+	city_names = city_name_map.keys()
+	job_names = job_title_map.keys()
+	
+	standardized_city_name = process.extractOne(city, city_names)[0]
+	standardized_job_name = process.extractOne(job, job_names)[0]
 
-#Pre-processing
-df = df[cols_to_use]
-df['outlier'] = df.apply(indicate_outlier_values, axis=1)
-df = df[df.outlier == 0]
-df['income'] = df.apply(get_income, axis=1)
-df.dropna(subset=['income'], inplace=True)
+	given = [city_name_map[standardized_city_name], job_title_map[standardized_job_name], education]
+
+	inputs = []
+	for field in fields:
+		if field in given:
+			print field
+			inputs.append(1)
+		else:
+			inputs.append(0)
+
+	age = float(age)
+	age_std = (age - 16) / (85 - 16)
+	inputs[fields.index('age')] = age_std
+
+	inputs[fields.index('female')] = gender_map[gender]
+
+	return np.dot(inputs, coefficients) * 1.5
 
 
-df.drop(['wage1', 'wage2', 'wage3', 'wage4', 'weekpay', 'pehrusl1'], axis=1, inplace=True)
 
-df.to_csv('final_df.csv')
-
-#df.educ.map(educ_map, inplace=True)
-#educ_dummies = pd.get_dummies(df.educ)
 
